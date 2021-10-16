8032e1b8139d846e0f1f43e6918ac0ea3d21fdf5
Sumon <sumon@error-inside.local>
2021-10-16 13:43:00 +0600

    custom types

--- src/index.js
+++ src/index.js
@@ -3,21 +3,48 @@
 
 import { GraphQLServer } from "graphql-yoga";
 
+//Scalar type: string, Boolean, Int, Float, ID
+// ! must return same scalar type
+// type name always cap
+
 const typeDefs= `
 type Query{
-    hello:String!
+    me: User!
+    post: Post!
+}
+
+type User{
+    id: ID!
     name: String!
+    email: String!
+    age: Int
+}
+type Post{
+    id:ID!
+    title: String!
+    body: String!
 }
 `
+//age value is not must
 
 const resolvers={
     Query:{
-        hello:()=>{
-            return `hello world`
+        me:()=>{
+            return{
+                id:"12345",
+                name: "jalmal",
+                email: "ja@gmail.com",
+                age: null
+            }
         },
-        name:()=>{
-            return `shamim`
+        post:()=>{
+            return{
+                id: "12345",
+                title:"I am ok with it",
+                body:"Why I am failed"
+            }
         }
+
     }
 }
 
@@ -25,3 +52,17 @@ const server = new GraphQLServer({typeDefs, resolvers})
 server.start(()=>console.log(`server is running`));
 
 //go to localhost 4000 & and query for graphql
+// query{
+//     me{
+//       id
+//       name
+//       email
+//       age
+//     }
+// post{
+//     id
+//     title
+//   }
+//   }
+  
+    
