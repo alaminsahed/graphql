7f1fc00bc3ed87fa28743921a9684d9cbad3af25
Sumon <sumon@error-inside.local>
2021-10-16 11:54:56 +0600

    intial server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+//schema
+//resolvers
+
+import { GraphQLServer } from "graphql-yoga";
+
+const typeDefs= `
+type Query{
+    hello:String!
+    name: String!
+}
+`
+
+const resolvers={
+    Query:{
+        hello:()=>{
+            return `hello world`
+        },
+        name:()=>{
+            return `shamim`
+        }
+    }
+}
+
+const server = new GraphQLServer({typeDefs, resolvers})
+server.start(()=>console.log(`server is running`));
+
+//go to localhost 4000 & and query for graphql
