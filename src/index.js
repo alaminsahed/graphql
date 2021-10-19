96b72593449efa4ca698fef648d227c8e28a4583
Sumon <sumon@error-inside.local>
2021-10-19 17:35:58 +0600

    array init

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import { GraphQLServer } from "graphql-yoga";
 
 const typeDefs= `
 type Query{
+    greeting(greeterName: String): String!
+    add(a:Float!, b:Float!): String!
+    sum(number:[Float]!): Float!
+    number: [Int]!
     me: User!
     post: Post!
 }
@@ -26,9 +30,26 @@ type Post{
 }
 `
 //age value is not must
+//resolvers get four args parent,args,ctx,info
 
 const resolvers={
     Query:{
+        sum:(parent,args,ctx,info)=>{
+            if(args.number.length === 0){
+                return 0;
+            }
+          return args.number.reduce((acc,value)=>{
+               return acc + value
+           })
+        },
+        add:(parent,args,ctx,info)=>{
+            let sum;
+            return sum= `${args.a}`+`${args.b}`
+        },
+        greeting:(_,args)=>{
+            console.log(args)
+            return `Konika said to ${args.greeterName}`
+        },
         me:()=>{
             return{
                 id:"12345",
@@ -43,8 +64,11 @@ const resolvers={
                 title:"I am ok with it",
                 body:"Why I am failed"
             }
+        },
+        number:()=>{
+            return [10,12,15]
         }
-
+       
     }
 }
 
